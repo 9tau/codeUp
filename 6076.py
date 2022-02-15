f7c18e9d81f97ceaf01f12f967e7aebcbc59f24e
9tau <86876756+9tau@users.noreply.github.com>
2022-02-15 23:03:47 +0900

    advance exercise1

--- 6076.py
+++ 6076.py
@@ -2,6 +2,6 @@ c = int(input())
 for i in range(c+1):  # 0~c까지 출력
     print(i)
 
-# range(끝)
+#range(끝)
 #range(시작, 끝)
 #range(시작, 끝, 증감)
---
+++ 6079.py
@@ -0,0 +1,8 @@
+n = int(input())
+a = 0
+c = 0
+while a < n:
+    c += 1
+    a += c
+
+print(c)
